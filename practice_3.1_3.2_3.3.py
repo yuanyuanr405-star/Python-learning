65c76ac267c677fdbce6f0709554f70ffdbaf2e0
yyr <yuanyuanr405@gmail.com>
2025-11-26 10:32:11 +0800

    2025.11.26日练习
    最新一次练习！

---
+++ practice_3.1_3.2_3.3.py
@@ -0,0 +1,13 @@
+names=['ryan','mike','john','lisa']
+print(names[0].title())
+print(names[1].title())
+print(names[-2].title())
+print(names[-1].title())
+message=f"{names[0].title()},how are you today?"
+print(message)
+message=f"{names[1].title()},how are you today?"
+print(message)
+message=f"{names[-2].title()},how are you today?"
+print(message)
+message=f"{names[-1].title()},how are you today?"
+print(message)
